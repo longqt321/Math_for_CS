83b956d88b3edde81dcc39d3d942a876de90e740
longqt321 <longqt321@gmail.com>
2025-01-08 01:08:54 +0700

    overload * operator for big num

--- primes.cpp
+++ primes.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
 #include <math.h>
 #include <vector>
+#include <string>
 using namespace std;
+
+string operator*(string a, string b){
+    int n = a.size();
+    int m = b.size();
+    if (a=="0"||b=="0") return "0";
+    vector<long long>vt(m+n,0);
+    for (int i=n-1;i>=0;--i){
+        for (int j=m-1;j>=0;--j){
+            long long mul = (a[i]-'0')*(b[j]-'0');
+            long long sum = mul+vt[i+j+1];
+            vt[i+j+1] = sum%10;
+            vt[i+j] += sum/10;
+        }
+    }
+    string res = "";
+    for (int x : vt){
+        if (!res.empty() || x > 0) res += (x+'0');
+    }
+    return (res.empty() ? "0" : res);
+}
 
 long long power(long long a,long long n){
     long long res = 1;
@@ -13,7 +34,7 @@ long long power(long long a,long long n){
     return res;
 }
 
-void factorize(long long n,vector<int>& p,vector<int>& a){
+void factorize(long long n,vector<long long>& p,vector<long long>& a){
     long long sqr = sqrt(n);
     for (int i=2;i<=sqr;++i){
         if (n%i==0){
@@ -43,10 +64,10 @@ int div_count(long long n){
     return cnt;
 }
 
-vector<int> get_div_list(long long n){
-    vector<int>vt;
+vector<long long> get_div_list(long long n){
+    vector<long long>vt;
     long long sqr = sqrt(n);
-    for (int i=2;i<=sqr;++i){
+    for (int i=1;i<=sqr;++i){
         if (n%i==0) vt.push_back(i);
     }
     int sz = vt.size();
@@ -57,7 +78,7 @@ vector<int> get_div_list(long long n){
 }
 
 long long div_sum(long long n){
-    vector<int>p,a;
+    vector<long long>p,a;
     factorize(n,p,a);
     long long sum=1;
     int sz = p.size();
@@ -67,10 +88,12 @@ long long div_sum(long long n){
     return sum;
 }
 
-long long div_product(long long n){
-    int divcount = div_count(n);
-    long long result = power(n,div_count(n)/2);
-    if (divcount%2!=0) result *= sqrt(n);
+string div_product(long long n){
+    vector<long long>divList = get_div_list(n);
+    string result = "1";
+    for (int i=0;i<divList.size();++i){
+        result = result*to_string(divList[i]);
+    }
     return result;
 }
 
@@ -90,7 +113,7 @@ bool new_check_perfect(long long n){
     return n == div_sum(n) - n;
 }
 
-int estimate_primes(long long n){
+long long estimate_primes(long long n){
     return n/log(n);
 }
 
@@ -116,7 +139,7 @@ int main(){
     long long n;
     cout << "Nhap n: ";
     cin >> n;
-    vector<int>p,a;
+    vector<long long>p,a;
     factorize(n,p,a);
     if (p.size() != a.size()){
         cout << "Error: Kich thuoc cua mang p va a khong giong nhau\n";
@@ -124,40 +147,40 @@ int main(){
     }
     int sz = p.size();
     cout << "Cau 1: \n";
-    cout << "Phan tich thua so nguyen to cua n: ";
+    cout << "Phan tich thua so nguyen to cua " << n << " la: ";
     for (int i=0;i<sz;++i){
         cout << p[i] << "^" << a[i];
         if (i<sz-1) cout << " * ";
     }
     cout << "\nCau 2: \n";
-    vector<int>divList = get_div_list(n);
+    vector<long long>divList = get_div_list(n);
     long long divcount = div_count(n);
-    cout << "Tong so uoc cua n: " << divcount << '\n';
+    cout << "Tong so uoc cua " << n << " la: "<< divcount << '\n';
     cout << "Danh sach cac uoc cua n: ";
     for (int i=0;i<divList.size();++i){
         cout << divList[i] << ' ';
     }
     cout << "\nCau 3: \n";
-    cout << "Tong cua cac uoc so la: " << div_sum(n) << '\n';
+    cout << "Tong cua cac uoc so cua " << n << " la: " << div_sum(n) << '\n';
     cout << "Cau 4: \n";
-    long long divproduct = div_product(n);
-    cout << "Tich cua cac uoc so la: " << divproduct  << '\n';
+    string divproduct = div_product(n);
+    cout << "Tich cua cac uoc so la: "  << n << " la: " << divproduct  << '\n';
     cout << "Cau 5: \n";
-    cout << "Ham check so hoan hao truyen thong: " << (old_check_perfect(n) ? "YES\n" : "NO\n");
-    cout << "Ham check so hoan hao theo cong thuc: " << (new_check_perfect(n) ? "YES\n" : "NO\n");
-    cout << "Nhap L va R de tinh so luong so hoan hao tron doan [L,R]\n";
+    cout << "Ham check "<< n <<" co phai la so hoan hao truyen thong: " << (old_check_perfect(n) ? "YES\n" : "NO\n");
+    cout << "Ham check "<< n <<" co phai la so hoan hao truyen thong: " << (new_check_perfect(n) ? "YES\n" : "NO\n");
+    cout << "Nhap L va R de tinh so luong so hoan hao trong doan [L,R]\n";
     long long l,r;
     cin >> l >> r;
-    cout << "Danh sach so hoan hao trong doan [L,R] la: ";
+    cout << "Danh sach so hoan hao trong doan [" << l <<","<< r <<"] la: ";
     for (int i=l;i<=r;++i){
         if (old_check_perfect(i)==true){
             cout << i << ' ';
         }
     }
     cout << "\nCau 6: \n";
-    cout << "Mat do so nguyen to tu 1->N la: " << estimate_primes(n) << '\n';
+    cout << "Mat do so nguyen to tu 1->" << n << " la: " << estimate_primes(n) << '\n';
     vector<int>primes = get_primes(l,r);
-    cout << "Danh sach so nguyen to tu L->R la: \n";
+    cout << "Danh sach so nguyen to trong doan [" << l <<","<< r <<"] la: \n";
     for (int i=0;i<primes.size();++i){
         cout << primes[i] << ' ';
     }
